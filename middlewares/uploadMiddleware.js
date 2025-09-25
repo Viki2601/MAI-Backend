01ad83deecb52d58ba343e1dae893a8a030ca2bf
Viki2601 <mcvicky2601@gmail.com>
2025-09-25 06:28:26 +0530

    Fixing

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,18 +1,15 @@
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
-
-// Ensure uploads folder exists
-if (!fs.existsSync('./uploads')) fs.mkdirSync('./uploads');
 
 // Storage config
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, './uploads'),
+    destination: (req, file, cb) => cb(null, '/tmp'),
     filename: (req, file, cb) => {
         const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, uniqueName + path.extname(file.originalname));
     }
 });
+
 
 const upload = multer({ storage });
 
